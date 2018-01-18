4653ad55105122fc605ebe4ce34a359ae34429ec
Clayton Campbell <breezeplz@gmail.com>
2018-01-18 16:54:44 -0500

    yup

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
+//waits until the DOM is loaded, then runs this script.
 $(document).ready(function() {
 
-    $.ajax({
+    $.ajax({ // when loaded, make the ajax request to the api and retrieve all "friends"
         url: 'http://rest.learncode.academy/api/WhatMatters/friends',
         type: 'GET'
-    }).done( (data) => {
-        data.forEach((eachFriend) => {
+    }).done( (data) => { //when the array comes back, iterate over it
+        data.forEach((eachFriend) => { //for each friend, create the full name and build it in the list-item
             let name = eachFriend.first + " " + eachFriend.last
-            let newElement = createFriend(name)
-            $('#friend-list').append(newElement)
+            let newElement = createFriend(name, eachFriend.id)
+            $('#friend-list').append(newElement) //puts the list into a list
         })
-    }).fail( () => {
+    }).fail( () => { //if unable to comply, says following statement
         alert('Failed, unable to retrieve.')
     })
 
-    $('#friend-form').submit( (e) => {
-        e.preventDefault()
+    $('#friend-form').submit( (e) => { //wehn loaded, make the click event for the add button
+        e.preventDefault() //stops the page from refreshing, then gets value from input
         let newFriend = $('#input').val()
-
+//splits the name based on first space found
         let first = newFriend.substr(0, newFriend.indexOf(' '))
         let last = newFriend.substr(newFriend.indexOf(' ')+1)
 
-        $.ajax({
+        $.ajax({ //sends the info to the API
             url: 'http://rest.learncode.academy/api/WhatMatters/friends',
             type: 'POST',
             data: {
@@ -28,23 +29,30 @@ $(document).ready(function() {
                 last: last,
                 email: `${first.toLowerCase()}@${last.toLowerCase()}.com`
             }
-        }).done( (data) => {
-            console.log(data)
-        }).fail( () => {
+        }).done( (data) => { // on success, the input is created and added to the list
+            let newElement = createFriend(newFriend, data.id)
+        $('#friend-list').append(newElement) //clears the input field
+        $('#input').val('')
+        }).fail( () => { //alert on fail
             alert('Failed, unable to post.')
         })
 
-        let newElement = createFriend(newFriend)
-        $('#friend-list').append(newElement)
-        $('#input').val('')
+    })
+    $('#friend-list').on('click', 'li', function(e) { //when list-item is clicked, do the function
+        $.ajax({ // ajax delete upon request from API, gets id from click devent details
+            url: 'http://rest.learncode.academy/api/WhatMatters/friends',
+          type: 'DELETE'
+        }).done( () => { // on success, remove the list item
+            $(this).remove()
+        }).fail( () => {
+            alert('Delete failed.')
+    })
+       
     })
 
-    $('#friend-list').on('click', 'li', function(e) {
-        $(this).remove()
-    })
-
-    function createFriend(name) {
-        return $(`<li>${name}</li>`).addClass('list-group-item list-group-item-action list-group-item-dark')
+    function createFriend(name, id) { //this creates the list html element for the input for the given name/id
+        //applies CSS classes to the element as well accrding to style
+        return $(`<li>${name}</li>`).addClass('list-group-item list-group-item-action list-group-item-dark').attr('id',id)
     }
 
 })
